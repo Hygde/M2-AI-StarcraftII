3bc14255124aba64936eed63986863476153ee08
Hygde <samuel.devals@outlook.fr>
2019-01-08 18:23:23 +0100

    Selecting a SCV, build a supply depot, and collecting minerals

--- Actions/builder.py
+++ Actions/builder.py
@@ -1,6 +1,51 @@
 from Actions.sc2actions import SC2Action
 
+from pysc2.lib import features, actions, units
+
 class Builder(SC2Action):
+
+    _PLAYER_NEUTRAL = features.PlayerRelative.NEUTRAL
+
+    #terrans unit id
+    _TERRAN_COMMANDCENTER = 18
+    _TERRAN_SCV = 45
+
+    _BUILD_SUPPLYDEPOT = actions.FUNCTIONS.Build_SupplyDepot_screen.id
+    _BUILD_BARRAQUEMENT = actions.FUNCTIONS.Build_Barracks_screen.id
 
     def __init__(self, base_top_left):
         super(Builder,self).__init__(base_top_left)
+        self.duration = 3
+
+    def _selectSCV(self, obs):
+        self.logger.debug("Selecting a SCV")
+        yx = (obs.observation["feature_screen"][self._UNIT_TYPE] == self._TERRAN_SCV).nonzero()
+        target = [yx[1][0], yx[0][0]]
+        return actions.FunctionCall(self._SELECT_POINT, [self._NOT_QUEUED, target])
+
+    def _buildSupplyDepot(self, obs):#todo generalize this function
+        self.logger.debug("")
+        unit_type = obs.observation["feature_screen"][self._UNIT_TYPE]
+        yx = (unit_type == self._TERRAN_COMMANDCENTER).nonzero()
+        target = self._transformLocation(int(yx[1].mean()), 0, int(yx[0].mean()), 20)
+        return actions.FunctionCall(self._BUILD_SUPPLYDEPOT, [self._NOT_QUEUED, target])
+
+    def _freeSCV(self, obs):
+        self.logger.debug("Freeing the SCV")
+        yx_min = (obs.observation["feature_screen"][self._UNIT_TYPE] == self.NEUTRAL_MINERALFIELD).nonzero()
+        self.logger.debug(yx_min);input()
+        target = self._transformLocation(int(yx_min[1].mean()), 0, int(yx_min[0].mean()), 0)
+        return actions.FunctionCall(self._MOVE_TO, [self._QUEUED, target])
+
+    def action(self, obs):
+        if self.iteration == 0:
+            result = self._selectSCV(obs)
+            self.iteration += 1
+        elif self._BUILD_SUPPLYDEPOT in obs.observation["available_actions"] and self.iteration < 2:
+            result = self._buildSupplyDepot(obs)
+            self.iteration +=1
+        elif self.iteration == 2:
+            self.iteration += 1
+            result = self._freeSCV(obs)
+        else:result = actions.FUNCTIONS.no_op()
+        return result
--- agent.py
+++ agent.py
@@ -1,13 +1,16 @@
 from pysc2.agents import base_agent
-from pysc2.lib import actions
-from pysc2.lib import features
+from pysc2.lib import actions, features
 
 import logging
 
 from Actions.sc2actions import SC2Action
 from Actions.builder import Builder
 
-#python -m pysc2.bin.agent --agent agent.Agent --map Simple64 --agent_race terran
+# List of known unit types. It is taken from:
+# https://github.com/Blizzard/s2client-api/blob/master/include/sc2api/sc2_typeenums.h
+
+# How to run:
+# python -m pysc2.bin.agent --agent agent.Agent --map Simple64 --agent_race terran
 
 class Agent(base_agent.BaseAgent):
     def __init__(self):
@@ -20,20 +23,25 @@ class Agent(base_agent.BaseAgent):
         ch.setLevel(logging.DEBUG)
         ch.setFormatter(formatter)
         self.logger.addHandler(ch)
-        self.iteration = 0
+        self.act = None
 
     def getBasePosition(self, obs):
-        if (self.iteration == 0) :
-            player_pos = (obs.observation["feature_minimap"][features.SCREEN_FEATURES.player_relative.index] == 1).nonzero()
-            self.base_top_left = player_pos[0].mean() <= 31
-            self.logger.debug("The main base is on the "+ ("top left" if self.base_top_left else "bottom right") + " corner")
+        player_pos = (obs.observation["feature_minimap"][features.SCREEN_FEATURES.player_relative.index] == 1).nonzero()
+        self.base_top_left = player_pos[0].mean() <= 31
+        self.logger.debug("The main base is on the "+ ("top left" if self.base_top_left else "bottom right") + " corner")
 
     def init(self, obs):
         self.getBasePosition(obs)
     
     def step(self, obs):
         super(Agent, self).step(obs)
-        if self.iteration == 0:self.init(obs)
-        act = SC2Action(self.base_top_left)
-        self.iteration += 1
-        return act.action(obs)
+        self.logger.debug("steps = "+str(self.steps))
+        self.logger.debug(actions.FUNCTIONS_AVAILABLE)
+        result = actions.FUNCTIONS.no_op()
+        if self.steps == 1:
+            self.init(obs)
+            self.act = Builder(self.base_top_left)
+        elif not self.act.isFinished():
+            result = self.act.action(obs)
+
+        return  result
