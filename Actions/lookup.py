a438d557f8415a429efe741074fd5eb4766f39d6
Hygde <samuel.devals@outlook.fr>
2019-01-14 21:15:33 +0100

    It performs a scan of the map, and returns to main base

--- Actions/lookup.py
+++ Actions/lookup.py
@@ -8,6 +8,7 @@ class LookUp(SC2Action):
 
     def __init__(self, base_top_left):
         super(LookUp, self).__init__(base_top_left)
+        self.duration = 10
 
     def getPositionOfTheMainBase(self, obs):
         unit_type = obs.observation["feature_screen"][self._UNIT_TYPE]
@@ -17,3 +18,16 @@ class LookUp(SC2Action):
     def moveCamera(self, x, y):
         self.logger.debug(self._MOVE_CAMERA)
         return actions.FUNCTIONS.move_camera([x, y])
+
+    def action(self, obs):
+        if self.iteration < 9:
+            x, y = 20 * int(self.iteration/3) + 12, 20 * (self.iteration % 3) + 12
+            self.logger.debug("x = "+str(x)+" y = "+str(y))
+        else:
+            if self.base_top_left: x, y = 18, 24
+            else: x, y = 40, 46
+        self.iteration += 1
+        return self.moveCamera(x,y)
+
+
+        
