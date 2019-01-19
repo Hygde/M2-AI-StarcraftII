5860fc90fea1bac96cbe1ac8906f749aa08ca0a1
Hygde <samuel.devals@outlook.fr>
2019-01-19 12:14:54 +0100

    Using python standards & dynamic check of the building type

--- Actions/builder.py
+++ Actions/builder.py
@@ -3,50 +3,62 @@ from Actions.sc2actions import SC2Action
 from pysc2.lib import features, actions, units
 
 class Builder(SC2Action):
-
+    #private fields
     _PLAYER_NEUTRAL = features.PlayerRelative.NEUTRAL
+    _TERRAN_SCV = 45#terrans unit id
 
-    #terrans unit id
-    _TERRAN_SCV = 45
+    #public fields
+    BUILD_SUPPLYDEPOT = actions.FUNCTIONS.Build_SupplyDepot_screen.id
+    BUILD_BARRACKS = actions.FUNCTIONS.Build_Barracks_screen.id
+    BUILD_ENGINEERINGBAY = actions.FUNCTIONS.Build_EngineeringBay_screen.id
+    BUILD_AUTOTURRET = actions.FUNCTIONS.Build_MissileTurret_screen.id
+    BUILD_BUNKER = actions.FUNCTIONS.Build_Bunker_screen.id
 
-    _BUILD_SUPPLYDEPOT = actions.FUNCTIONS.Build_SupplyDepot_screen.id
-    _BUILD_BARRAQUEMENT = actions.FUNCTIONS.Build_Barracks_screen.id
-    _BUILD_AUTOTURRET = actions.FUNCTIONS.Build_MissileTurret_screen.id
-    _BUILD_BUNKER = actions.FUNCTIONS.Build_Bunker_screen.id
-
-    def __init__(self, base_top_left, building=_BUILD_SUPPLYDEPOT):
+    def __init__(self, base_top_left, buildings, to_build=BUILD_SUPPLYDEPOT):
         super(Builder,self).__init__(base_top_left)
-        self.duration = 3
-        self.to_build = building
+        self._duration = 4
+        self._to_build = to_build
+        self._buildings = buildings
 
     def _selectSCV(self, obs):
-        self.logger.debug("Selecting a SCV")
+        self._logger.debug("Selecting a SCV")
         yx = (obs.observation["feature_screen"][self._UNIT_TYPE] == self._TERRAN_SCV).nonzero()
         target = [yx[1][0], yx[0][0]]
         return actions.FunctionCall(self._SELECT_POINT, [self._QUEUED, target])
 
-    def _buildSupplyDepot(self, obs):#todo generalize this function
-        self.logger.debug("")
+    def _offsetByBuildingType(self, btype):
+        if   btype == self.BUILD_SUPPLYDEPOT:      x, y = 0 * self._buildings[btype]    , 5 * self._buildings[btype] + 15
+        elif btype == self.BUILD_BARRACKS:         x, y = 5 * self._buildings[btype] + 15, 5 * self._buildings[btype] + 15
+        elif btype == self.BUILD_ENGINEERINGBAY:   x, y = 5 * self._buildings[btype] + 15, 0 * self._buildings[btype]
+        return (x, y)
+
+    def _build(self, obs):#todo generalize this function
+        self._logger.debug("")
         unit_type = obs.observation["feature_screen"][self._UNIT_TYPE]
         yx = (unit_type == self._TERRAN_COMMANDCENTER).nonzero()
-        target = self._transformLocation(int(yx[1].mean()), 0, int(yx[0].mean()), 20)
-        return actions.FunctionCall(self.to_build, [self._QUEUED, target])
+        xy_off = self._offsetByBuildingType(self._to_build)
+        self._logger.debug(xy_off); input()
+        target = self._transformLocation(int(yx[1].mean()), xy_off[0], int(yx[0].mean()), xy_off[1])
+        return actions.FunctionCall(self._to_build, [self._QUEUED, target])
 
-    def _freeSCV(self, obs):
-        self.logger.debug("Freeing the SCV")
+    def _extractMineral(self, obs):
+        self._logger.debug("Freeing the SCV")
         yx_min = (obs.observation["feature_screen"][self._UNIT_TYPE] == self.NEUTRAL_MINERALFIELD).nonzero()
         target = self._transformLocation(int(yx_min[1].mean()), 0, int(yx_min[0].mean()), 0)
         return actions.FunctionCall(self._MOVE_TO, [self._QUEUED, target])
 
     def action(self, obs):
-        if self.iteration == 0:
+        if self._iteration == 0:
             result = self._selectSCV(obs)
-            self.iteration += 1
-        elif self._BUILD_SUPPLYDEPOT in obs.observation["available_actions"] and self.iteration < 2:
-            result = self._buildSupplyDepot(obs)
-            self.iteration +=1
-        elif self.iteration == 2:
-            self.iteration += 1
-            result = self._freeSCV(obs)
+            self._iteration += 1
+        elif self._to_build in obs.observation["available_actions"] and self._iteration < 2:
+            result = self._build(obs)
+            self._iteration +=1
+        elif self._iteration == 2:
+            self._iteration += 1
+            result = self._extractMineral(obs)
+        elif self._iteration == 3:
+            result = self._selectSCV(obs)
+            self._iteration += 1
         else:result = actions.FUNCTIONS.no_op()
         return result
--- agent.py
+++ agent.py
@@ -5,6 +5,7 @@ import logging
 
 from Actions.sc2actions import SC2Action
 from Actions.builder import Builder
+from Actions.lookup import LookUp
 
 # List of known unit types. It is taken from:
 # https://github.com/Blizzard/s2client-api/blob/master/include/sc2api/sc2_typeenums.h
@@ -15,33 +16,46 @@ from Actions.builder import Builder
 class Agent(base_agent.BaseAgent):
     def __init__(self):
         super(Agent, self).__init__()
-        self.logger = logging.getLogger()
-        self.logger = logging.getLogger()
-        self.logger.setLevel(logging.DEBUG)
+        self._logger = logging.getLogger()
+        self._logger = logging.getLogger()
+        self._logger.setLevel(logging.DEBUG)
         formatter = logging.Formatter("%(asctime)s :: %(levelname)s :: %(module)s :: %(funcName)s :: %(message)s")
         ch = logging.StreamHandler()
         ch.setLevel(logging.DEBUG)
         ch.setFormatter(formatter)
-        self.logger.addHandler(ch)
-        self.act = None
+        self._logger.addHandler(ch)
+        self._act = None
+
+    def reset(self):
+        super(Agent, self).reset()
+        self.steps = 0
 
     def getBasePosition(self, obs):
         player_pos = (obs.observation["feature_minimap"][features.SCREEN_FEATURES.player_relative.index] == 1).nonzero()
         self.base_top_left = player_pos[0].mean() <= 31
-        self.logger.debug("The main base is on the "+ ("top left" if self.base_top_left else "bottom right") + " corner")
+        self._logger.debug("The main base is on the "+ ("top left" if self.base_top_left else "bottom right") + " corner")
+        self.built = False
 
     def init(self, obs):
         self.getBasePosition(obs)
+        self.base_position = LookUp(self.base_top_left).getPositionOfTheMainBase(obs)
+        self._logger.debug(self.base_position)
+        self.buildings = {Builder.BUILD_SUPPLYDEPOT:0, Builder.BUILD_BARRACKS:0, Builder.BUILD_ENGINEERINGBAY:0, Builder.BUILD_BUNKER:0, Builder.BUILD_AUTOTURRET:0}
+        self._act = Builder(self.base_top_left, self.buildings, Builder.BUILD_SUPPLYDEPOT)
     
     def step(self, obs):
         super(Agent, self).step(obs)
-        self.logger.debug("steps = "+str(self.steps))
-        self.logger.debug(actions.FUNCTIONS_AVAILABLE)
+        self._logger.debug("steps = "+str(self.steps))
         result = actions.FUNCTIONS.no_op()
         if self.steps == 1:
             self.init(obs)
-            self.act = Builder(self.base_top_left)
-        elif not self.act.isFinished():
-            result = self.act.action(obs)
+            #self._act = LookUp(self.base_top_left)
+        elif not self._act.isFinished():
+            result = self._act.action(obs)
+        else:
+            self.buildings[Builder.BUILD_SUPPLYDEPOT] += 1
+            if not self.built:
+                self._act = Builder(self.base_top_left, self.buildings, Builder.BUILD_BARRACKS)
+                self.built = True
 
-        return  result
+        return result
